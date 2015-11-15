1c27b3dc5f22a680d23ee5b5247e97034036121c
alexgil1994 <alexgil2000@hotmail.com>
2015-11-15 18:51:30 +0200

    Profile path in AccountResource to get the data.
    Needs the profile.html as string in the return and the register code

--- src/main/java/snapichatdreamteam/resources/AccountResource.java
+++ src/main/java/snapichatdreamteam/resources/AccountResource.java
@@ -124,6 +124,56 @@ public class AccountResource {
         return "You have successfully register. Please sign in";
     }
     
+    @POST
+    @Path("/profile")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getUserProfile(string userName)  {
+      
+        RegisterModel rm = new RegisterModel(InputStreamToBYtes(image), fName, lName, email, phone, city, country, uName, pass, conPass); //new register for profilegetdata
+        
+        //TODO: Add the connection string to a configuration file
+        //TODO: Move all this database stuff to repository classes
+        Connection con = null;
+           
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/snapchatdb","root","zkpovolz01");
+            
+            String SQL_SELECT = "SELECT username, user_phone_num, user_city, user_city, user_country FROM user WHERE username = 'nat';";
+            PreparedStatement statement = con.prepareStatement(SQL_SELECT);
+                 statement.setString(1, rm.getUsername());
+                 
+                try {
+                   statement.setString(2, PasswordHash.createHash(rm.getPass())); 
+                   
+                } catch (NoSuchAlgorithmException ex) {
+                   
+                } catch (InvalidKeySpecException ex) {
+                  
+                }
+                 
+                 statement.setString(3, rm.getEmail());
+                 statement.setString(4, rm.getPhone());
+                 statement.setString(5, rm.getCity());
+                 statement.setString(6, rm.getCountry());
+                 statement.setBytes(7, rm.getImage());
+                 statement.setString(8, rm.getfName());
+                 statement.setString(9, rm.getlName());
+                 statement.executeUpdate();
+            } catch (SQLException ex) {
+               //TODO: Add logging
+            }finally{
+                if (con!=null){
+                    try {
+                        con.close();
+                    } catch (SQLException ex) {
+                        
+                    }
+                }
+            }
+            
+        return ""; // make the profile.html as string in the ""
+    }
     
     private byte[] InputStreamToBYtes(InputStream is){
         ByteArrayOutputStream outpuStream = null;
